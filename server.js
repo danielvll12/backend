163b2c962601ab78f14688bad0210e8e8c81eef6
daniel valle <danielvallec98@gmail.com>
2025-06-25 16:13:48 -0600

    actualice

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+
 
 const carsFile = './data/cars.json';
 let cars = [];
